a395902f614a4c50dd51534b1241070820f31c96
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-22 20:50:48 -0700

    initializing the github for my DadJokesAPI

---
+++ script/app.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("display");
+let getDadBtn = document.getElementById("getDadBtn");
+
+const dadUrl = "https://icanhazdadjoke.com/";
+
+async function getDaddy(){
+    // stuff inside to do a fetch and apiResponse to then save to json format
+    
+}
+
+getDadBtn.addEventListener("click", function(){
+    getDaddy();
+});
