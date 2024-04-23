313bf71db30fec1375f129f9a1a8d728813d828a
dljimenez2324 <dljimenez2324@gmail.com>
2024-04-22 21:50:09 -0700

    added the header parameter and arrow function used to save to json data

--- script/app.js
+++ script/app.js
@@ -3,11 +3,16 @@ let getDadBtn = document.getElementById("getDadBtn");
 
 const dadUrl = "https://icanhazdadjoke.com/";
 
-async function getDaddy(){
-    // stuff inside to do a fetch and apiResponse to then save to json format
-    
-}
-
 getDadBtn.addEventListener("click", function(){
     getDaddy();
 });
+
+async function getDaddy(){
+    // stuff inside to do a fetch and apiResponse to then save to json format
+    let apiResponse = await fetch(dadUrl, {
+        headers: {
+            Accept:"application/json",
+        },
+    }).then(Response => Response.json());
+    console.log(apiResponse.joke);
+};
